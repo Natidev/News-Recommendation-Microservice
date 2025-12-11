659dc2d53e1024806dd8261be11533003f52d260
Natidev <53361365+Natidev@users.noreply.github.com>
2025-12-11 20:18:32 +0300

    Minor fixes

--- back/user-service/src/main/java/com/ds/user_service/model/User.java
+++ back/user-service/src/main/java/com/ds/user_service/model/User.java
@@ -2,11 +2,17 @@ package com.ds.user_service.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
 
 @Entity
+@Getter
+@Setter
 public class User {
 @Id
     String username;
 String password;
-String role;
+List<String> role;
 }
--- back/user-service/src/main/resources/db/migration/V1_init_userdb.sql
+++ back/user-service/src/main/resources/db/migration/V1_init_userdb.sql
@@ -1,5 +1,5 @@
 create table if not exists users(
     username varchar(50) primary key ,
     password varchar,
-    role varchar
+    role varchar[]
 )
