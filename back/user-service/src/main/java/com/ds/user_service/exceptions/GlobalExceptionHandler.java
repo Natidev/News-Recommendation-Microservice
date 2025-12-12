d7fba7dc84ad3021f0936acdde716bc594fdc9d7
Natidev <53361365+Natidev@users.noreply.github.com>
2025-12-13 00:09:47 +0300

    Update GlobalExceptionHandler.java

--- back/user-service/src/main/java/com/ds/user_service/exceptions/GlobalExceptionHandler.java
+++ back/user-service/src/main/java/com/ds/user_service/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.ds.user_service.exceptions;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -11,18 +12,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler( UserAlreadyExistsException.class)
     ResponseEntity<?> handleDuplicateUser(UserAlreadyExistsException e){
+        log.error(e.getMessage(),e);
         return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
     }
     @ExceptionHandler( UserNotFoundException.class)
     ResponseEntity<?> handleUserNotFound(UserNotFoundException e){
+        log.error(e.getMessage(),e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        log.error(ex.getMessage(),ex);
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult()
                 .getAllErrors()
@@ -32,5 +37,10 @@ public class GlobalExceptionHandler {
                                 errors.put(((FieldError)error).getField(), error.getDefaultMessage())
                 );
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleException(Exception ex){
+        log.error(ex.getMessage(),ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
     }
 }
