306232ecd2a768e195164c9199883a29a387ac57
Natidev <53361365+Natidev@users.noreply.github.com>
2025-12-14 00:21:36 +0300

    upgraded and configured swagger

--- back/user-service/src/main/java/com/ds/user_service/configurations/SecurityConfig.java
+++ back/user-service/src/main/java/com/ds/user_service/configurations/SecurityConfig.java
@@ -45,6 +45,12 @@ public AuthenticationManager  authenticationManager(
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers(
+                                "/auth/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**")
+                        .permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
