8bc5ebabee340f0ae5184acf8e3a553d3e7fb923
Natidev <53361365+Natidev@users.noreply.github.com>
2025-12-17 23:19:44 +0300

    Updated Repository

--- back/user-service/src/main/java/com/ds/user_service/repository/UserRepository.java
+++ back/user-service/src/main/java/com/ds/user_service/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.ds.user_service.repository;
 
 import com.ds.user_service.model.User;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends R2dbcRepository<User, String>
+public interface UserRepository extends ReactiveCrudRepository<User, String>
 {
 }
