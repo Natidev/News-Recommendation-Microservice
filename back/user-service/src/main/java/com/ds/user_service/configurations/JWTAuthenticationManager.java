95be8aedafd31b9483bf0e30dd5381f80a2195bf
Natidev <53361365+Natidev@users.noreply.github.com>
2025-12-18 03:10:27 +0300

    Updated AuthManager

--- back/user-service/src/main/java/com/ds/user_service/configurations/JWTAuthenticationManager.java
+++ back/user-service/src/main/java/com/ds/user_service/configurations/JWTAuthenticationManager.java
@@ -1,35 +1,38 @@
 package com.ds.user_service.configurations;
 
 import com.ds.user_service.repository.UserRepository;
-import com.ds.user_service.service.AuthService;
 import com.ds.user_service.service.JwtService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.server.authentication.ServerAuthenticationConverter;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class JWTAuthenticationManager implements ReactiveAuthenticationManager {
     private final JwtService jwtService;
-    private final UserRepository repository;
+
     @Override
     public Mono<Authentication> authenticate(Authentication authentication) throws AuthenticationException {
         String token = authentication.getCredentials().toString();
         String username = jwtService.getUserName(token);
-
-        return repository.findById(username)
-                .map(userDetails -> {
-                    if (jwtService.validateToken(token)) {
-                        return authentication;
-                    } else {
-                        throw new AuthenticationException("Invalid JWT token") {};
-                    }
-                });
+        if (username != null && jwtService.validateToken(token)) {
+            var auth = new PreAuthenticatedAuthenticationToken(
+                    username,
+                    null,
+                    List.of()
+            );
+            return Mono.just(auth);
+        }else  {
+            return Mono.empty();
+        }
     }
 
 }
